e5109e3c5bae4a0641bd0bbd111acb11ebeaf150
anonymous-oracle <Suhass149@gmail.com>
2021-11-12 20:52:52 +0530

    fixed db bug

--- alembic/versions/1770a3098c85_add_users_table.py
+++ alembic/versions/1770a3098c85_add_users_table.py
@@ -21,7 +21,7 @@ def upgrade():
     op.create_table(
         "users",
         sa.Column("id", sa.Integer, primary_key=True, nullable=False),
-        sa.Column("email", sa.Integer, nullable=False, unique=True),
+        sa.Column("email", sa.String, nullable=False, unique=True),
         sa.Column("password", sa.String, nullable=False),
         sa.Column("salt", sa.String, nullable=False),
         sa.Column(
