1cce3424bcd9f366baebda9a39669a070e5bcdc4
anonymous-oracle <Suhass149@gmail.com>
2021-11-23 21:25:18 +0530

    basic Class testing

--- app/calculations.py
+++ app/calculations.py
@@ -12,3 +12,20 @@ def multiply(num1: int, num2: int):
 
 def divide(num1: int, num2: int):
     return num1 / num2
+
+
+class BankAccount:
+    def __init__(self, starting_balance=0):
+        self._balance = starting_balance
+
+    def deposit(self, amount):
+        self._balance += amount
+
+    def withdraw(self, amount):
+        self._balance -= amount
+
+    def collect_interest(self):
+        self._balance *= 1.1
+
+    def get_balance(self):
+        return self._balance
--- test/my_test.py
+++ test/my_test.py
@@ -1,12 +1,19 @@
-from app.calculations import add, divide, multiply, subtract
+from app.calculations import BankAccount, add, divide, multiply, subtract
+import pytest
 
 
-def test_add():  # fundtions should be named test* and classes as Test*
+@pytest.mark.parametrize(
+    "num1, num2, expected", [(5, 3, 8), (4, 3, 7), (5, 6, 11), (500, 30, 530)]
+)
+def test_add(
+    num1, num2, expected
+):  # fundtions should be named test* and classes as Test*
     print("testing add function")  # to print this line use 'pytest -s' command
-    assert add(5, 3) == 8
-    assert add(4, 3) == 7
-    assert add(5, 6) == 11
-    assert add(500, 30) == 530
+    # assert add(5, 3) == 8
+    # assert add(4, 3) == 7
+    # assert add(5, 6) == 11
+    # assert add(500, 30) == 530
+    assert add(num1, num2) == expected
 
 
 def test_subtract():
@@ -26,3 +33,31 @@ def test_divide():
     assert divide(1, 1) == 1
     assert divide(9, 3) == 3
     assert divide(5, 2) == 2.5
+
+
+def test_bank_set_initial_amount():
+    bank_account = BankAccount(50)
+    assert bank_account.get_balance() == 50
+
+
+def test_bank_default_amount():
+    bank_account = BankAccount()
+    assert bank_account.get_balance() == 0
+
+
+def test_bank_withdraw():
+    account = BankAccount(100)
+    account.withdraw(50)
+    assert account.get_balance() == 50
+
+
+def test_bank_deposit():
+    account = BankAccount(100)
+    account.deposit(50)
+    assert account.get_balance() == 150
+
+
+def test_bank_collect_interest():
+    account = BankAccount(100)
+    account.collect_interest()
+    assert round(account.get_balance()) == 110
